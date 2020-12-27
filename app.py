5be5279c3bef3c2b12ba0b1e2ed2f5d84e1ad656
Josh <thejoshuataylor@hotmail.com>
2020-12-27 19:08:59 +0000

    filter updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 def data_load():
 	df = pd.read_pickle('Data/data.pkl')
 	df['idx'] = df.index
-	df['y1'] = df['idx']/df.shape[0]*5
+	df['y1'] = 1-(df['idx']/df.shape[0])
 	return df
 st.title('EU, UK & NI deal')
 st.subheader('AI exploration of the EU, UK and Northern Island agreement')
@@ -19,19 +19,29 @@ with st.beta_expander("What is this chart showing?"):
      st.write("""
          The chart below shows all of the paragraphs of text contained within the
          UK and EU deal agreed on the 24th December 2020.
-         Colours in the chart represent the different sections of the deal (see below).
+         Colours in the chart represent the different sections of the deal you can see what they relate to by 
+         hovering over a point.
          Viewing the information by 'order of agreement', will show the document from start to end
          (top-to-bottom) with items arranged in the horizontal axis by topics.
          Viewing the information by 'subject clusters' will cluster the paragraphs in the document
          by how similar they are.
-         You can pan an zoom into the chart. Hovering over a paragraph will display the text. 
+         You can pan an zoom into the chart. Hovering over a point will display the text. 
 
      """)
-     st.image('legend.png')
 
 
 
-
+st.sidebar.header("Filters:")
+Search = st.sidebar.text_input('Search for keywords')
+if len(Search)>0:
+	Search = Search.lower()
+	df = df.loc[df['Text'].str.lower().str.contains(Search)]
+Parts = st.sidebar.multiselect('Select an area of the agreement', df.Parts.unique())
+if len(Parts)>0:
+	df = df.loc[df['Parts'].isin(Parts)]
+Titles = st.sidebar.multiselect('Select a chapter of the agreement', df.Titles.unique())
+if len(Titles)>0:
+	df = df.loc[df['Titles'].isin(Titles)]
 
 render = st.radio('Select view',['Render by order of agreement','Render by subject clusters'])
 
@@ -40,6 +50,11 @@ if render == 'Render by subject clusters':
 else:
 	df_output = df[['z','y1','idx','Titles','Words']]
 
+
+# Articles = st.sidebar.multiselect('Select an article of the agreement', df.Articles.unique())
+# if len(Articles)>0:
+# 	mask = df['Parts'].isin(Articles)
+# 	df = df[mask]
 
 df_output.columns = ['x','y','idx','Titles','Words']
 
@@ -76,18 +91,4 @@ except:
 		st.write(i[1])
 	pass
 
-st.sidebar.header("Filters:")
-Search = st.sidebar.text_input('Search for keywords')
-if len(Search)>0:
-	Search = Search.lower()
-	df = df.loc[df['Text'].str.lower().str.contains(Search)]
-Parts = st.sidebar.multiselect('Select an area of the agreement', df.Parts.unique())
-if len(Parts)>0:
-	df = df.loc[df['Parts'].isin(Parts)]
-Titles = st.sidebar.multiselect('Select a chapter of the agreement', df.Titles.unique())
-if len(Titles)>0:
-	df = df.loc[df['Titles'].isin(Titles)]
-# Articles = st.sidebar.multiselect('Select an article of the agreement', df.Articles.unique())
-# if len(Articles)>0:
-# 	mask = df['Parts'].isin(Articles)
-# 	df = df[mask]
+
